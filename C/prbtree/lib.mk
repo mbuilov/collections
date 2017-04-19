d16c725a5c4cb270c49af6f6873b4c0c51a10bce
Michael Builov <mbuilov@gmail.com>
2017-04-19 18:32:06 +0300

    fix LIBRARY_HEADERS list

--- C/prbtree/lib.mk
+++ C/prbtree/lib.mk
@@ -26,7 +26,7 @@ LIB := $(if $(NO_STATIC),,$(PRBTREE_LIB_NAME) $(PRBTREE_LIB_VARIANTS))
 
 LIBRARY_NAME := prbtree
 LIBRARY_HDIR :=
-LIBRARY_HEADERS := btree.h prbtree.h
+LIBRARY_HEADERS := include/btree.h include/prbtree.h
 
 define PRBTREE_PC_COMMENT
 Author:  $(VENDOR_NAME)
