e7724b4969707b932e9797fd23582742ce0973ec
Michael M. Builov <mbuilov@gmail.com>
2021-08-04 13:14:37 +0300

    add copyright notice

--- Java/tests/PtreeTest.java
+++ Java/tests/PtreeTest.java
@@ -1,3 +1,9 @@
+/**********************************************************************************
+* Embedded binary tree of nodes with parent pointers
+* Copyright (C) 2012-2021 Michael M. Builov, https://github.com/mbuilov/collections
+* Licensed under LGPL version 3 or any later version, see COPYING
+**********************************************************************************/
+
 import emcollections.Ptree.*;
 
 public class PtreeTest {
